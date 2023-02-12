f2abba23e005f3dd6cbffb4483ad8f8084b84c8b
Noah Byers <noahxbyers@gmail.com>
2023-02-12 15:05:01 -0800

    Finished iteration 3 and 4, as well as read me

--- README.md
+++ README.md
@@ -1,46 +1,44 @@
-# Hang In There
 
-A boilerplate repo. 
-
-## Set Up
-
-1. One teammate: fork this repository
-2. Clone down your new, forked repo
-3. cd into the repository
-4. Open it in your text editor
-5. Add all project partners and your assigned instructor as collaborators on the repository
-
-Project spec & rubric can be found [here](https://frontend.turing.edu/projects/module-1/hang-in-there.html)
-
-To view your project:
-
-1. In your terminal, navigate to your project repo
-2. Run the command `open index.html`
-  
-______________________________________________________  
-# README Template  
-Before turning this project in, erase this line and everything above it and fill in the info below.  
 ______________________________________________________  
 
 # Hang in There  
 
 ### Abstract:
 [//]: <> (Briefly describe what you built and its features. What problem is the app solving? How does this application solve that problem?)
+This application helps give users random inspritaitonal posters, as well as give users the ability to create their own posters using text fields and copying image addresses into URL Format. Another feature includes saving both random and personally created poster in a grid to look at. The user can also delete saved posters by double clicking on the saved "mini" posters.
 
 ### Installation Instructions:
 [//]: <> (What steps does a person have to take to get your app cloned down and running?)
+1. Open the terminal and navigate to where you’d like to store the repository
+ 2. Fork this repository [here](https://github.com/SleepyisAwak3/hang-in-there-boilerplate) or use this deployable [link](https://sleepyisawak3.github.io/hang-in-there/)
+ 3. Clone down your new, forked repo into the directory of your choice using:
+git clone <url>
+ 1. cd into the repository
+ 2. Open in your text editor using code .
+ 3. Open the window of the application by running open index.html into your terminal.
 
 ### Preview of App:
 [//]: <> (Provide ONE gif or screenshot of your application - choose the "coolest" piece of functionality to show off.)
-
+![Screenshot 2023-02-12 at 2 20 14 PM](https://user-images.githubusercontent.com/115435434/218340768-ff2c6ad2-04df-4dc4-8424-14665255dc8a.png)
 ### Context:
 [//]: <> (Give some context for the project here. How long did you have to work on it? How far into the Turing program are you?)
+We were assigned the project on 2/7 and had until 2/12 to complete the project, so we had three full weekdays with classes and two weekend days to complete the project. One of the partners is two weeks into Mod1 and one is repeating Mod1, but also two weeks into the project.
 
 ### Contributors:
 [//]: <> (Who worked on this application? Link to their GitHubs.)
+[Caitlin](https://github.com/caitlincradick) 
 
+[Noah](https://github.com/SleepyisAwak3)
 ### Learning Goals:
 [//]: <> (What were the learning goals of this project? What tech did you work with?)
+The non-technical learning goals of the project were to create an understanding of how to psuedocode and develop professional work habits such as working with a partner and following the norms set at the beginning of the project. Technically, the goals were to learn and display knowledge of the DOM, Javascript style/ readability and OOP (class, instances, etc) competency. Git and Gitflow was a major part of this as well when it comes to partner pairing and making our pull requests, etc. follow convention. Lastly, we displayed our knowledge in the functionality of the project.
+The tech interfaces that we worked with were VS Code, the Terminal, GitHub. We worked with Javascript with attempting to understand HTML and CSS in the DOM.
 
 ### Wins + Challenges:
 [//]: <> (What are 2-3 wins you have from this project? What were some challenges you faced - and how did you get over them?)
+Wins: 
+1. One win we had was getting the Poster information to display and be saved in the DOM. We originally had it functional but had no way of making sure it updated to the actual arrays. After a little bit of psuedo-coding and breaking down what we need to do into a function we accomplished our goal.
+1. Another win we had was getting the ability to delete the saved posters. We ended up using a double click event listener. It was an issue as how to get rid of just a single poster and also have the ability to not use a single click. We found the solution quick after a nicely worded google search that helped it click for us.
+
+Challenges:
+1. The main challenge we had was the team work aspect in terms of workflow and experience difference. With one of us being a repeater it made it challenging to balance out work and make sure both parties got the discovery and work in that they needed in order to truly understand what was needed to be done or learned in order to succeed in our site matching the iteration goals. We ended up doing both live coding driver navigator sessions and also working alone and comparing notes at the end of the day. 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-// .query selector variables go here 👇 HEAD
-//iteration 0
 var posterQuote = document.querySelector(".poster-quote");
 var posterTitle = document.querySelector(".poster-title");
 var posterImage = document.querySelector(".poster-img");
 var randomPosterButton = document.querySelector(".show-random");
-//iteration 1 -
 var hideMainPosterPage = document.querySelector(".main-poster");
 var formCreatePoster = document.querySelector(".poster-form");
 var showMyPosterButton = document.querySelector(".make-poster");
@@ -16,10 +13,9 @@ var imageUrlInput = document.querySelector("#poster-image-url");
 var backToMainButton = document.querySelector(".back-to-main");
 var showSavedPageButton = document.querySelector('.show-saved');
 var showSavedPosters = document.querySelector('.saved-posters');
+var saveThisPosterGrid = document.querySelector('.saved-posters-grid');
+var saveThisPosterButton = document.querySelector('.save-poster');
 
-//iteration 2
-
-// we've provided you with some data to work with 👇
 var images = [
   "./assets/bees.jpg",
   "./assets/bridge.jpg",
@@ -119,68 +115,99 @@ var quotes = [
 ];
 var savedPosters = [];
 var currentPoster;
-
-// event listeners go here 👇
-//Iteration 0
-window.addEventListener('load', homePage);
-randomPosterButton.addEventListener('click', homePage);
-//Iteration 1
-showFormButton.addEventListener('click', openFormPage);
-takeMeBack.addEventListener('click', openMainPage);
-showMyPosterButton.addEventListener('click', displayMyPoster);
-formCreatePoster.addEventListener('load', openFormPage);
-backToMainButton.addEventListener('click', backToMainPage);
-showSavedPageButton.addEventListener('click', openSavedPosterPage);
-//functions and event handlers go here 👇
-
 var imgIndex = getRandomIndex(images);
 var quoteIndex = getRandomIndex(quotes);
 var titleIndex = getRandomIndex(titles);
 
-//iteratrion 0
-function homePage() {
+window.addEventListener('load', showRandomPoster)
+randomPosterButton.addEventListener('click', showRandomPoster)
+showFormButton.addEventListener('click', openFormPage)
+takeMeBack.addEventListener('click', openMainPage)
+showMyPosterButton.addEventListener('click', displayMyPoster)
+formCreatePoster.addEventListener('load', openFormPage)
+backToMainButton.addEventListener('click', backToMainPage)
+showSavedPageButton.addEventListener('click', changeGrid)
+saveThisPosterGrid.addEventListener('dblclick', deleteSavedPoster)
+saveThisPosterButton.addEventListener('click', saveMyPoster)
 
-posterImage.src = images[getRandomIndex(images)]
-posterTitle.innerText = titles[getRandomIndex(titles)]
-posterQuote.innerText = quotes[getRandomIndex(quotes)]
+function showRandomPoster() {
+  var imgIndex = getRandomIndex(images);
+  var quoteIndex = getRandomIndex(quotes);
+  var titleIndex = getRandomIndex(titles);
+  var newPoster = {
+    image: images[imgIndex],
+    title: titles[titleIndex],
+    quote: quotes[quoteIndex],
+  }
+currentPoster = new Poster(newPoster.image, newPoster.title, newPoster.quote)
+posterImage.src = currentPoster.imageURL
+posterTitle.innerText = currentPoster.title
+posterQuote.innerText = currentPoster.quote
 }
 
-//Iteration 1
 function openFormPage(){
   hideMainPosterPage.classList.add('hidden')
   formCreatePoster.classList.remove('hidden')
 }
-
+//Iteration 1
   function openMainPage(){
     hideMainPosterPage.classList.remove('hidden')
     formCreatePoster.classList.add('hidden')
   }
 
-  function openSavedPosterPage() {
+  function displayMyPoster(event){
+    event.preventDefault()
+    formCreatePoster.classList.add('hidden')
+    hideMainPosterPage.classList.remove('hidden')
+    currentPoster = new Poster(imageUrlInput.value, posterTitleInput.value, posterQuoteInput.value)
+    posterImage.src = currentPoster.imageURL
+    posterTitle.innerText = currentPoster.title
+    posterQuote.innerText = currentPoster.quote
+    images.push(imageUrlInput)
+    titles.push(posterTitleInput)
+    quotes.push(posterQuoteInput)
+  }
+  function saveMyPoster() {
+    if (!savedPosters.includes(currentPoster)) {
+      savedPosters.push(currentPoster)
+    }
+  }
+
+function changeGrid() {
+    var displaySavedPosters = [];
+      for (var i = 0; i < savedPosters.length; i++) {
+        displaySavedPosters += `<article class="mini-poster" id="${savedPosters[i].id}">
+        <img class="poster-img" src="${savedPosters[i].imageURL}" alt="nothin' to see here">
+        <h2 class="poster-title">${savedPosters[i].title}</h2>
+        <h4 class="poster-quote">${savedPosters[i].quote}</h4></article>`
+        saveThisPosterGrid.innerHTML = displaySavedPosters
+    }
+    saveThisPosterPage()
+  }
+
+function openSavedPosterPage() {
   hideMainPosterPage.classList.add('hidden')
   showSavedPosters.classList.remove('hidden')
   }
-  function backToMainPage() {
+function backToMainPage() {
   hideMainPosterPage.classList.remove('hidden')
   showSavedPosters.classList.add('hidden')
   }
+function saveThisPosterPage() {
+  hideMainPosterPage.classList.add('hidden')
+  showSavedPosters.classList.remove('hidden')
+  //savedPosters.push(saveThisPosterButton)
+  }
 
-//Iteration 2
-function displayMyPoster(){
-  event.preventDefault();
-  formCreatePoster.classList.add('hidden')
-  hideMainPosterPage.classList.remove('hidden')
-  posterImage.src = imageUrlInput.value 
-  posterTitle.innerText = posterTitleInput.value 
-  posterQuote.innerText = posterQuoteInput.value 
-  images.push(imageUrlInput.value)
-  titles.push(posterTitleInput.value)
-  quotes.push(posterQuoteInput.value)
-}
+function deleteSavedPoster(event){
+    for (var i = 0; i < savedPosters.length; i++) {
+    if(savedPosters[i].id == event.target.parentNode.id)
+      savedPosters.splice(i, 1)
+      event.target.parentNode.remove(event.target)
+      }
+    changeGrid()
+  }
 // (we've provided one for you to get you started)!
-//function newRandomPoster() {
-
-
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
